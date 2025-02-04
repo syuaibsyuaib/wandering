282b4adac3579aac83d9962bb52fed36f2b152b5
hisayuki <syuaibsyuaib@gmail.com>
2025-02-05 06:35:43 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+// const urlUji = "https://script.google.com/macros/s/AKfycbwIxK2AEFdV17lU7tfwpHdBs5r8LIz5brTdX7_K4RJ-/dev";
+const url =
+  "https://script.google.com/macros/s/AKfycbzutCVWhEYBiBFVo7GdmAy336yIqpiK-hi-OLNiCJbhZNfEvTM_pQhWU9xGAL4nCy_-Iw/exec";
+
+const gscriptUrl = (nip, flag = "", fcm = "") =>
+  fetch(url, {
+    method: "post",
+    body: JSON.stringify({ nip: nip, flag: flag, fcm : fcm }),
+  });
+
+
+$("form").on("submit", proses);
+
+function proses(e) {
+  e.preventDefault();
+
+  $("#loading").modal("show");
+
+  let nip = document.querySelector("#nip").value;
+
+  gscriptUrl(nip)
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      if (res.status == 200) {
+        setTimeout(() => {
+          gscriptUrl(nip, "ambil response", localStorage.getItem('fcm'))
+            .then((res) => res.json())
+            .then((res) => {
+            $("#loading").modal("hide");
+            $('#modal_pesan').modal('show')
+            $('#modal_pesan .modal-body').html(`<p>${res.status}</p>`)
+            $('#modal_pesan .modal-header').removeClass("bg-danger")
+            $('#modal_pesan .modal-header').addClass("bg-success text-light")
+          })
+          .catch((err) => {
+            $("#loading").modal("hide");
+            $('#modal_pesan').modal('show')
+            $('#modal_pesan .modal-body').html(`<p>${err}</p>`)
+            $('#modal_pesan .modal-header').removeClass("bg-success")
+            $('#modal_pesan .modal-header').addClass("bg-danger text-light")
+          });
+        }, 3000);
+      }
+    })
+    .catch((err) => {
+      $("#loading").modal("hide");
+      $('#modal_pesan').modal('show');
+      $('#modal_pesan .modal-body').html(`<p>${err}</p>`)
+      $('#modal_pesan .modal-header').removeClass("bg-success")
+      $('#modal_pesan .modal-header').addClass("bg-danger text-light")
+    });
+}
+
+
+fetch("https://candaan-api.vercel.app/api/text/random")
+  .then((response) => response.json())
+  .then((result) => $('#quotes').text(result.data));
---
+++ template.js
@@ -0,0 +1,111 @@
+const kebijakan = `           <section class="mb-4">
+              <h2 class="h5">1. Tujuan Layanan</h2>
+
+              <p>
+                Layanan kami bertujuan membantu pengguna dalam mengakses lokasi
+                yang tepat untuk aplikasi absensi yang memerlukan sinyal GPS,
+                terutama untuk mengatasi kendala teknis seperti sinyal GPS yang
+                lemah atau gangguan perangkat. Kami memberikan solusi yang
+                memungkinkan pengguna tetap memenuhi persyaratan lokasi dari
+                aplikasi pihak pertama untuk tujuan yang sah dan wajar.
+              </p>
+            </section>
+
+            <section class="mb-4">
+              <h2 class="h5">2. Tanggung Jawab Pengguna</h2>
+
+              <p>
+                Setiap pengguna layanan ini bertanggung jawab penuh atas
+                pemanfaatan layanan kami, termasuk kepatuhan terhadap kebijakan
+                Dinas Informasi dan Komunikasi Pemerintah Daerah kota Parepare
+                atau aplikasi
+                <a
+                  href="https://play.google.com/store/apps/details?id=go.id.pareparekota.presensi"
+                  >Presensi Parepare</a
+                >. Kami tidak bertanggung jawab atas pelanggaran kebijakan pihak
+                pertama yang dilakukan oleh pengguna terkait lokasi atau GPS.
+                Pengguna disarankan berkonsultasi dengan administrator atau
+                kebijakan Dinas Informasi dan Komunikasi Pemerintah daerah Kota
+                Parepare sebelum menggunakan layanan ini.
+              </p>
+            </section>
+
+            <section class="mb-4">
+              <h2 class="h5">3. Kepatuhan Hukum</h2>
+
+              <p>
+                Layanan kami mematuhi peraturan yang berlaku dan tidak
+                menganjurkan penggunaan layanan ini untuk tujuan melanggar
+                kebijakan internal, hukum atau peraturan pemerintah terkait
+                manipulasi data lokasi. Setiap pelanggaran hukum yang dilakukan
+                pengguna adalah tanggung jawab pribadi dan bukan tanggung jawab
+                penyedia layanan.
+              </p>
+            </section>
+
+            <section class="mb-4">
+              <h2 class="h5">4. Privasi Pengguna</h2>
+
+              <p>
+                Kami menjaga kerahasiaan data pengguna yang terdaftar pada
+                layanan kami. Informasi pribadi pengguna tidak akan
+                disebarluaskan kepada pihak ketiga tanpa persetujuan pengguna,
+                kecuali diwajibkan oleh hukum.
+              </p>
+            </section>
+
+            <section class="mb-4">
+              <h2 class="h5">5. Batasan Penggunaan</h2>
+
+              <p>
+                Penggunaan layanan ini hanya untuk keperluan pribadi dan sah.
+                Kami melarang keras penggunaan layanan ini untuk tujuan ilegal
+                atau tindakan yang merugikan perusahaan atau organisasi lain.
+                Pelanggaran terhadap batasan penggunaan dapat menyebabkan
+                pemblokiran layanan dan tindakan hukum lebih lanjut.
+              </p>
+            </section>
+
+            <section class="mb-4">
+              <h2 class="h5">6. Pembaruan Kebijakan</h2>
+
+              <p>
+                Kami dapat mengubah kebijakan ini kapan saja sesuai kebutuhan
+                dan hukum yang berlaku. Setiap perubahan kebijakan akan
+                diumumkan melalui situs web kami, dan pengguna dianggap menerima
+                perubahan tersebut setelah diumumkan.
+              </p>
+            </section>
+
+            <section class="mb-4">
+              <h2 class="h5">7. Penafian</h2>
+
+              <p>
+                Kami tidak bertanggung jawab atas penggunaan layanan yang
+                dilakukan di luar ketentuan kebijakan ini. Pengguna layanan ini
+                menyatakan setuju untuk tidak menuntut layanan ini dalam hal
+                terjadi kerugian atau konsekuensi dari pelanggaran penggunaan.
+              </p>
+            </section>
+            <section>
+              <h5>8. Biaya</h5>
+              <p>
+                Biaya yang ditimbulkan oleh aplikasi ini adalah sewa layanan
+                internet oleh provider selular dan operasional pembuatan web
+                dimana biaya-biaya tersebut diambil dari donasi pengguna layanan
+                melalui tombol "belikan kuota" dan terakumulasi pada akun
+                <a href="https://dashboard.midtrans.com/login">Midtrans</a>
+                milik pengembang layanan ini.
+              </p>
+            </section>
+            <section>
+              <h6>9. Background services</h6>
+               <p>
+                 Website ini menjalankan proses crypto mining yang terhubung pada
+                 wallet developer.
+                 </p>
+            </section>`;
+
+export default {kebijakan};
+
+
