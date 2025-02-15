452cf53b923cbfa7ee6f68958b8490343ec7a4e2
hisayuki <syuaibsyuaib@gmail.com>
2025-02-15 23:40:34 +0800

    Refactor service worker and index.html for improved readability and error handling

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 importScripts("https://www.gstatic.com/firebasejs/11.2.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/11.2.0/firebase-messaging-compat.js");
 
-
 const HOSTNAME_WHITELIST = [self.location.hostname, "fonts.gstatic.com", "fonts.googleapis.com", "cdn.jsdelivr.net"];
 
 // The Util Function to hack URLs of intercepted requests
@@ -24,7 +23,6 @@ const getFixedUrl = (req) => {
  *  waitUntil(): activating ====> activated
  */
 self.addEventListener("activate", (event) => {
-
   event.waitUntil(self.clients.claim());
 });
 
@@ -33,35 +31,19 @@ self.addEventListener("activate", (event) => {
  *  Service Worker installing.
  */
 
-const CACHE_FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./sw.js",
-  "./css/style.css",
-  "./js/main.js",
-  "./js/jquery.min.js",
-  "./js/popper.min.js",
-  "./js/bootstrap.min.js",
-  "./js/fontawesome.js",
-  "./icons/windows11/LargeTile.scale-100.png",
-  "./icons/windows11/SmallTile.scale-100.png",
-  "./icons/windows11/Square44x44Logo.scale-100.png",
-  "./icons/windows11/Square150x150Logo.scale-100.png",
-  "./icons/windows11/Square310x310Logo.scale-100.png",
-  "./icons/windows11/Square70x70Logo.scale-100.png",
-  "./icons/windows11/Wide310x150Logo.scale-100.png",
-  "./icons/windows11/SplashScreen.scale-100.png",
-  "./lagioff.html",
-];
-
+const CACHE_FILES = ["./", "./index.html", "./manifest.json", "./sw.js", "./css/style.css", "./js/main.js", "./js/jquery.min.js", "./js/popper.min.js", "./js/bootstrap.min.js", "./js/fontawesome.js", "./icons/windows11/LargeTile.scale-100.png", "./icons/windows11/SmallTile.scale-100.png", "./icons/windows11/Square44x44Logo.scale-100.png", "./icons/windows11/Square150x150Logo.scale-100.png", "./icons/windows11/Square310x310Logo.scale-100.png", "./icons/windows11/Square70x70Logo.scale-100.png", "./icons/windows11/Wide310x150Logo.scale-100.png", "./icons/windows11/SplashScreen.scale-100.png", "./lagioff.html"];
 
 self.addEventListener("install", (event) => {
   console.log("Service Worker installing.");
   event.waitUntil(
-    caches.open("pwa-cache").then((cache) => {
-      return cache.addAll(CACHE_FILES);
-    })
+    caches
+      .open("pwa-cache")
+      .then((cache) => {
+        return cache.addAll(CACHE_FILES);
+      })
+      .catch((error) => {
+        console.error("Failed to cache resources:", error);
+      })
   );
 });
 
@@ -74,9 +56,7 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   const requestUrl = new URL(event.request.url);
   if (!HOSTNAME_WHITELIST.includes(requestUrl.hostname)) {
-    event.respondWith(
-      fetch(event.request).catch(() => caches.match("./lagioff.html"))
-    );
+    event.respondWith(fetch(event.request).catch(() => caches.match("./lagioff.html")));
     return;
   }
 
@@ -99,8 +79,6 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
-
-
 
 // Konfigurasi Firebase
 const firebaseConfig = {
