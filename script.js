51807a929d4faaddfd44b586c1063c317f3b31c1
hisayuki <syuaibsyuaib@gmail.com>
2026-01-03 16:00:55 +0800

    refactor(payment): extract payment logic into separate function
    Extract Snap payment handling into payWithSnap function to improve code reuse and maintainability. Add better error handling and pending state support for payment flow.

--- script.js
+++ script.js
@@ -22,13 +22,20 @@ function proses(e) {
   gscriptUrl(nip)
     .then((res) => res.json())
     .then((res) => {
-      console.log(res);
-      if(res.status == 'ilegal'){
+      console.log("First response:", res);
+      if (res.status == 'ilegal') {
         $("#loading").modal("hide");
         $('#modal_pesan').modal('show')
         $('#modal_pesan .modal-body').html(`<p>Buang mengkudu buah manggis (cakep...), \n Sedekah dulu pake Qris. xixixi</p>`)
         $('#modal_pesan .modal-header').removeClass("bg-success")
         $('#modal_pesan .modal-header').addClass("bg-danger text-light")
+        return;
+      }
+
+      if (res.status == 'belum bayar') {
+        $("#loading").modal("hide");
+        payWithSnap(res);
+        return;
       }
 
       if (res.status == 200) {
@@ -36,38 +43,25 @@ function proses(e) {
           gscriptUrl(nip, "ambil response", localStorage.getItem('fcm'))
             .then((res) => res.json())
             .then((res) => {
-            $("#loading").modal("hide");
-            if(res.status == 'belum bayar'){
-              snap.pay(res.token, {
-                onSuccess: function(result){
-                  $("#loading").modal("hide");
-                  $('#modal_pesan').modal('show')
-                  $('#modal_pesan .modal-body').html(`<p>${res.status}</p>`)
-                  $('#modal_pesan .modal-header').removeClass("bg-danger")
-                  $('#modal_pesan .modal-header').addClass("bg-success text-light")
-                },
-                onError: function(result){
-                  $("#loading").modal("hide");
-                  $('#modal_pesan').modal('show')
-                  $('#modal_pesan .modal-body').html(`<p>${res.status}</p>`)
-                  $('#modal_pesan .modal-header').removeClass("bg-success")
-                  $('#modal_pesan .modal-header').addClass("bg-danger text-light")
-                }
-              })
-            }
-            $('#modal_pesan').modal('show')
-            $('#modal_pesan .modal-body').html(`<p>${res.status}</p>`)
-            $('#modal_pesan .modal-header').removeClass("bg-danger")
-            $('#modal_pesan .modal-header').addClass("bg-success text-light")
-          })
-          .catch((err) => {
-            alert(err)
-            $("#loading").modal("hide");
-            $('#modal_pesan').modal('show')
-            $('#modal_pesan .modal-body').html(`<p>Coba cek di aplikasi aslinya, kayaknya sudah berhasilmi</p>`)
-            $('#modal_pesan .modal-header').removeClass("bg-success")
-            $('#modal_pesan .modal-header').addClass("bg-danger text-light")
-          });
+              console.log("Second response:", res);
+              $("#loading").modal("hide");
+              if (res.status == 'belum bayar') {
+                payWithSnap(res);
+              } else {
+                $('#modal_pesan').modal('show')
+                $('#modal_pesan .modal-body').html(`<p>${res.status}</p>`)
+                $('#modal_pesan .modal-header').removeClass("bg-danger")
+                $('#modal_pesan .modal-header').addClass("bg-success text-light")
+              }
+            })
+            .catch((err) => {
+              alert(err)
+              $("#loading").modal("hide");
+              $('#modal_pesan').modal('show')
+              $('#modal_pesan .modal-body').html(`<p>Coba cek di aplikasi aslinya, kayaknya sudah berhasilmi</p>`)
+              $('#modal_pesan .modal-header').removeClass("bg-success")
+              $('#modal_pesan .modal-header').addClass("bg-danger text-light")
+            });
         }, 5000);
       }
     })
@@ -79,6 +73,42 @@ function proses(e) {
       $('#modal_pesan .modal-header').removeClass("bg-success")
       $('#modal_pesan .modal-header').addClass("bg-danger text-light")
     });
+}
+
+function payWithSnap(res) {
+  if (typeof snap !== 'undefined') {
+    snap.pay(res.token, {
+      onSuccess: function (result) {
+        $("#loading").modal("hide");
+        $('#modal_pesan').modal('show')
+        $('#modal_pesan .modal-body').html(`<p>Pembayaran Berhasil!</p>`)
+        $('#modal_pesan .modal-header').removeClass("bg-danger").addClass("bg-success text-light")
+      },
+      onPending: function (result) {
+        $("#loading").modal("hide");
+        $('#modal_pesan').modal('show')
+        $('#modal_pesan .modal-body').html(`<p>Menunggu Pembayaran...</p>`)
+        $('#modal_pesan .modal-header').removeClass("bg-success").addClass("bg-warning text-dark")
+      },
+      onError: function (result) {
+        $("#loading").modal("hide");
+        $('#modal_pesan').modal('show')
+        $('#modal_pesan .modal-body').html(`<p>Pembayaran Gagal!</p>`)
+        $('#modal_pesan .modal-header').removeClass("bg-success").addClass("bg-danger text-light")
+      },
+      onClose: function () {
+        $("#loading").modal("hide");
+      }
+    });
+  } else {
+    $("#loading").modal("hide");
+    $('#modal_pesan').modal('show');
+    $('#modal_pesan .modal-body').html(`
+      <p>Sistem pembayaran tidak termuat. Anda dapat membayar melalui tautan berikut:</p>
+      <a href="${res.redirect_url}" target="_blank" class="btn btn-primary w-100">Bayar Sekarang</a>
+    `);
+    $('#modal_pesan .modal-header').removeClass("bg-success").addClass("bg-warning text-dark");
+  }
 }
 
 
